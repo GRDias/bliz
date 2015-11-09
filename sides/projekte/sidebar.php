8cfccc4f8c8454192c9197147dea0be87889235e
markuslisa <markuslisa@markuslisa-book.fritz.box>
2015-11-09 17:28:10 +0100

    schnarch .... fix

--- sides/projekte/sidebar.php
+++ sides/projekte/sidebar.php
@@ -1,4 +1,6 @@
-<div id="sidebar">
+		</div>
+	</div>
+	<div id="sidebar">
 		<ul class="sidebox whiteBox">
 			<li><h3><a href="projekte.php?d1=Projekte&d2=">Projekte</a></h3></li>
 			<li><a href="checkliste_dinslaken.php?d1=Projekte&d2=Checkliste Dinslaken">Checkliste Bauen für alle barrierefrei der Stadt Dinslaken</a></li>
@@ -12,4 +14,5 @@
 			<li><a href="geldboerse_fuer_blinde.php?d1=Projekte&d2=Geldbörse für Blinde">Geldbörse für Blinde</a></li>
 			<li><a href="jobline_reha.php?d1=Projekte&d2=">Jobline Reha</a></li>
 		</ul>
+	</div>
 </div>
