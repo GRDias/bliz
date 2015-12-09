5d598de7373c4fc333c9b92b03cb5f91730bee56
MarkKrauss <markuskrauss@ymail.com>
2015-12-09 14:38:23 +0100

    Content Studium (Style,Text fix)

--- sides/studium/main.php
+++ sides/studium/main.php
@@ -3,9 +3,9 @@
 			
 	<div>
 		<h1>Studieren in Gießen - und das BliZ begleitet Sie dabei...</h1>
-		<img src="" alt="" />
+		<img src="bliz/img/bilder/giessen.jpg" alt="bild der Stadt Gießen" class="img_giessen" />
 		<p>
-			Das Studium an einer Hochschule ist der erste große Schritt in die Selbständigkeit vieler behinderter und chronisch erkrankter (Sehschwäche, Legasthenie, Angstpatienten, Hörschwäche, Autismus, Psychische Erkrankungen...) Menschen, die zum größten Teil aus einer Schule kommen, an der vieles für sie geregelt wurde. Wo finde ich die Information, wo finden meine Vorlesungen statt oder wie schaffe ich es, dass ich meiner Behinderung gerechte Lernmaterialien bekomme? Diesen und vielen anderen Fragen haben wir uns angenommen und begleiten unsere Studierenden vom Studieneinstieg bis zum Studienabschluss und darüber hinaus ... . Was wir auch für sie tun können, entnehmen Sie bitte unserem BliZ-Flyer. Auch für behinderte und chronisch kranke Studierende der Justus-Liebig-Universität sind wir da und bemühen uns, ihnen soweit wie möglich zu helfen. Weiterhin können Sie sich über unser Projekt Hessisches - elektronisches Lernportal für chronisch Kranke und Behinderte (H-eLB) informieren<br/>
+			Das Studium an einer Hochschule ist der erste große Schritt in die Selbständigkeit vieler behinderter und chronisch erkrankter (Sehschwäche, Legasthenie, Angstpatienten, Hörschwäche, Autismus, Psychische Erkrankungen...) Menschen, die zum größten Teil aus einer Schule kommen, an der vieles für sie geregelt wurde. Wo finde ich die Information, wo finden meine Vorlesungen statt oder wie schaffe ich es, dass ich meiner Behinderung gerechte Lernmaterialien bekomme? Diesen und vielen anderen Fragen haben wir uns angenommen und begleiten unsere Studierenden vom Studieneinstieg bis zum Studienabschluss und darüber hinaus ... . Was wir auch für sie tun können, entnehmen Sie bitte unserem BliZ-Flyer. Auch für behinderte und chronisch kranke Studierende der Justus-Liebig-Universität sind wir da und bemühen uns, ihnen soweit wie möglich zu helfen. Weiterhin können Sie sich über unser Projekt <a href="helb.php?d1=Projekte&d2=HeLB">Hessisches - elektronisches Lernportal für chronisch Kranke und Behinderte (H-eLB)</a> informieren<br/>
 			<br/>
 			Für Wünsche und Verbesserungsvorschläge von unseren Studenten haben wir immer ein offenes Ohr. Denn wir sind der Meinung, dass die Studenten selbst am besten wissen, was sie während ihres Studiums brauchen. Wir stellen den Studenten nur die allgemein benötigten Hilfsmittel wie Sprachausgabe, Braillezeile oder Bildschirmlesegeräte zur Verfügung. Über individuell benötigte Hilfsmittel machen wir uns mit den Studenten gemeinsam Gedanken und suchen zusammen nach einer Lösung.<br/>
 		</p>	
@@ -16,29 +16,13 @@
 		<p>
 			Nicht jeder ist zum Einzelkämpfer geboren. Natürlich kann man mit seiner Behinderung und chronischen Erkrankung versuchen ohne Hilfe zurecht zu kommen. Oftmals klappt das auch gut. Aber in den meisten Fällen führen kleine Hilfestellungen unsererseits zu großen Ergebnissen, die man selbst nicht erwartet hätte. Warum das Studium mit Barrieren unnötig verlängern, wenn man sie minimiert schneller und angenehmer bewältigen kann?<br/>
 			<br/>
-			!!! Deshalb zögern Sie nicht, uns zu kontaktieren !!!<br/>
-			<br/><br/> 
-			<strong> Ansprechpartner zum Studium mit Behinderung und chronischen Erkrankungen im BliZ</strong><br/>
+			<span class="center">!!! Deshalb zögern Sie nicht, uns zu <a href="kontakt.php?d1=Fachbereich&amp;d2=Kontakt&amp;d3=" titel="zu den Kontaktdaten">kontaktieren</a> !!!</span><br/>
+			<br/><br/>			
 		</p>
 	</div>
-	<br />
-	<div>
-		<h3>Folgenden finden Sie interessante Links zum Studium an der Technischen Hochschule Mittelhessen</h3>
 
-		<br/>Prüfungsamt: Hier finden Sie Informationen rund um Prüfungen in Ihrem Studium an der THM.<br/>
-		<br/>
-		Alle Studiengänge an der Technischen Hochschule Mittelhessen (THM) finden Sie hier.<br/>
-		<br/><br/>
-		<h4>Links zur Justus-Liebig-Universität Gießen</h4>
-
-		Alle Studiengänge an der Justus-Liebig-Universität finden Sie hier.<br/>
-		<br/>
-		Arbeitskreis Disability Studies (Studien zu oder über Behinderung). Die Disability Studies sind ein interdisziplinäres Forschungsfeld. Sie widmen sich historischen, ökonomischen, kulturellen, politischen, rechtlichen, psychologischen etc. Fragestellungen. Historiker/innen innerhalb der Disability Studies untersuchen z. B. die gesellschaftliche Stellung behinderter Menschen in verschiedenen historischen Epochen; Rechtswissenschaftler/innen erforschen die rechtliche Konstruktion von Behinderung.
-	</div>
-
+	
 <!-- ContentEnde -->
 <?php include "sides/studium/sidebar.php"; ?>
 
-<!-- Content ganze Breite-->
-	<div class="contentWidth">Test breite</div>
-<!-- Content ganze Breite Ende-->
+<?php include "footer_main.php" ?>
