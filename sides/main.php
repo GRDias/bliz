5d013d462411f9cf1cc6a98a18dddcbb51cd54dc
MarkKrauss <markuskrauss@ymail.com>
2016-01-01 12:16:28 +0100

    Startseite mit 3 Boxen ... Testweise

--- sides/main.php
+++ sides/main.php
@@ -1,42 +1,41 @@
-<div id="content">	
-	
-	<div id="contentLeft" role="main">
-		<div id="haupttext">
-			<h1>Herzlich willkommen auf den Seiten des BliZ</h1>
-			<hr />
-			<p>Das BliZ bietet für chronisch kranke und behinderte Studierende eine optimale Unterstützung. Unser Ziel ist die Verringerung von Barrieren im Studium. Durch individuelle Nachteilsausgleichsmaßnamen wollen wir den Studierenden ein reibungsloses Studium ermöglichen, so dass eine behinderungsbedingte Verlängerung oder gar ein Abbruch des Studiums vermieden werden kann.</p>
-			<br/>
-			<p>Unsere <strong>Angebote und Hilfen</strong> für <strong>Sie</strong>:</p>
-			<br/>
-			<ul class="squareDots">
-				<li>Persönliche Beratung / Coaching vor und während des Studiums</li>
-				<li>PC-Arbeitsräume mit Braillezeilen und Vergrößerungssoftware</li>
-				<li>Anpassung von Lehrunterlagen für rehabilitationstechnische Geräte</li>
-				<li>Durchführung von barrierefreien Klausuren</li>
-				<li>Hilfsmittelpool zur Ausleihe</li>
-				<li>Infoveranstaltungen für Schüler</li>
-				<li>Beratung / Information von Dozenten und Mitarbeitern der THM</li>
-				<li>Zimmervermittlung</li>			
-	    		<li>...</li>
-			</ul>
-		</div>
-		<div id="aktuelles">
-			<h1>Aktuelles</h1>
-			<?php include "aktuelles.php"; ?>
-		</div>			
-	</div>
-
-	<div id="sidebar">
-		<ul class="sidebox redBox">
-			<li><a href="profil.php?d1=Fachbereich&d2=Profil&d3="><h3>Nützliche Links</h3></a></li>
-		 	<li><a href="">Kontakt</a></li>
-		 	<li><a href="">So finden Sie uns</a></li>
-		 	<li><a href="">Studium</a></li>
-		 	<li><a href="">Team</a></li>
-		 	<li><a href="">Interessantes</a></li>		 	
-		</ul>		
-		
-		<?php include "sides/kontakt.php"; ?>
-	</div>
+<div role="main" id="welcometxt">
+	<h1>Herzlich willkommen auf den Seiten des BliZ</h1>
+	<hr />
+	<p>Das BliZ bietet für chronisch kranke und behinderte Studierende eine optimale Unterstützung. Unser Ziel ist die Verringerung von Barrieren im Studium. Durch individuelle Nachteilsausgleichsmaßnamen wollen wir den Studierenden ein reibungsloses Studium ermöglichen, so dass eine behinderungsbedingte Verlängerung oder gar ein Abbruch des Studiums vermieden werden kann.</p>
+	<br/>
+	<p>Unsere <strong>Angebote und Hilfen</strong> für <strong>Sie</strong>:</p>
+	<br/>
+	<ul class="squareDots">
+		<li>Persönliche Beratung / Coaching vor und während des Studiums</li>
+		<li>PC-Arbeitsräume mit Braillezeilen und Vergrößerungssoftware</li>
+		<li>Anpassung von Lehrunterlagen für rehabilitationstechnische Geräte</li>
+		<li>Durchführung von barrierefreien Klausuren</li>
+		<li>Hilfsmittelpool zur Ausleihe</li>
+		<li>Infoveranstaltungen für Schüler</li>
+		<li>Beratung / Information von Dozenten und Mitarbeitern der THM</li>
+		<li>Zimmervermittlung</li>			
+		<li>...</li>
+	</ul>
 </div>
 	
+<div id="mainfoot">
+	<div class="row">
+		<div class="news">			
+			<h1>Aktuelles</h1>			
+			<ul></ul>
+		</div>
+		<div class="links">
+			<ul>
+				<li><a href="profil.php?d1=Fachbereich&d2=Profil&d3="><h3>Nützliche Links</h3></a></li>
+			 	<li><a href="">Kontakt</a></li>
+			 	<li><a href="">So finden Sie uns</a></li>
+			 	<li><a href="">Studium</a></li>
+			 	<li><a href="">Team</a></li>
+			 	<li><a href="">Interessantes</a></li>		 	
+			</ul>
+		</div>
+		<div class="sponsors">
+			text
+		</div>
+	</div>
+</div>
