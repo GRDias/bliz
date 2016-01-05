48fb16f10ff18d24a2200b8e06555846d3484775
MarkKrauss <markuskrauss@ymail.com>
2016-01-05 12:29:38 +0100

    Alte Index-Main

--- sides/main.php
+++ sides/main.php
@@ -1,41 +1,96 @@
-<div role="main" id="welcometxt" id="content">
-	<h1>Herzlich willkommen auf den Seiten des BliZ</h1>
-	<hr />
-	<p>Das BliZ bietet für chronisch kranke und behinderte Studierende eine optimale Unterstützung. Unser Ziel ist die Verringerung von Barrieren im Studium. Durch individuelle Nachteilsausgleichsmaßnamen wollen wir den Studierenden ein reibungsloses Studium ermöglichen, so dass eine behinderungsbedingte Verlängerung oder gar ein Abbruch des Studiums vermieden werden kann.</p>
-	<br/>
-	<p>Unsere <strong>Angebote und Hilfen</strong> für <strong>Sie</strong>:</p>
-	<br/>
-	<ul class="squareDots">
-		<li>Persönliche Beratung / Coaching vor und während des Studiums</li>
-		<li>PC-Arbeitsräume mit Braillezeilen und Vergrößerungssoftware</li>
-		<li>Anpassung von Lehrunterlagen für rehabilitationstechnische Geräte</li>
-		<li>Durchführung von barrierefreien Klausuren</li>
-		<li>Hilfsmittelpool zur Ausleihe</li>
-		<li>Infoveranstaltungen für Schüler</li>
-		<li>Beratung / Information von Dozenten und Mitarbeitern der THM</li>
-		<li>Zimmervermittlung</li>			
-		<li>...</li>
-	</ul>
-</div>
+<div id="content">	
 	
-<div id="mainfoot">
-	<div class="row">
-		<div class="news">			
-			<h1>Aktuelles</h1>			
-			<ul></ul>
-		</div>
-		<div class="links">
-			<ul>
-				<li><a href="profil.php?d1=Bliz&amp;d2=Profil&amp;d3="><h3>Nützliche Links</h3></a></li>
-			 	<li><a href="">Kontakt</a></li>
-			 	<li><a href="">So finden Sie uns</a></li>
-			 	<li><a href="">Studium</a></li>
-			 	<li><a href="">Team</a></li>
-			 	<li><a href="">Interessantes</a></li>		 	
+	<div id="contentLeft" role="main">
+		<div id="haupttext">
+			<h1>Herzlich willkommen auf den Seiten des BliZ</h1>
+			<hr />
+			<p>Das BliZ bietet für chronisch kranke und behinderte Studierende eine optimale Unterstützung. Unser Ziel ist die Verringerung von Barrieren im Studium. Durch individuelle Nachteilsausgleichsmaßnamen wollen wir den Studierenden ein reibungsloses Studium ermöglichen, so dass eine behinderungsbedingte Verlängerung oder gar ein Abbruch des Studiums vermieden werden kann.</p>
+			<br/>
+			<p>Unsere <strong>Angebote und Hilfen</strong> für <strong>Sie</strong>:</p>
+			<br/>
+			<ul class="squareDots">
+				<li>Persönliche Beratung / Coaching vor und während des Studiums</li>
+				<li>PC-Arbeitsräume mit Braillezeilen und Vergrößerungssoftware</li>
+				<li>Anpassung von Lehrunterlagen für rehabilitationstechnische Geräte</li>
+				<li>Durchführung von barrierefreien Klausuren</li>
+				<li>Hilfsmittelpool zur Ausleihe</li>
+				<li>Infoveranstaltungen für Schüler</li>
+				<li>Beratung / Information von Dozenten und Mitarbeitern der THM</li>
+				<li>Zimmervermittlung</li>			
+	    		<li>...</li>
 			</ul>
 		</div>
-		<div class="sponsors">
-			text
+		<div id="aktuelles">
+			<h1>Aktuelles</h1>
+			<?php include "aktuelles.php"; ?>
+		</div>			
+	</div>
+
+	<div id="sidebar">
+		<ul class="sidebox redBox">
+			<li><a href="profil.php?d1=Fachbereich&d2=Profil&d3="><h3>Nützliche Links</h3></a></li>
+		 	<li><a href="">Kontakt</a></li>
+		 	<li><a href="">So finden Sie uns</a></li>
+		 	<li><a href="">Studium</a></li>
+		 	<li><a href="">Team</a></li>
+		 	<li><a href="">Interessantes</a></li>		 	
+		</ul>		
+		
+		<div class="sidebox greenBox infokontakt">
+			<table>
+				<thead>
+					<tr>
+						<th colspan="2">
+							<p>Kontakt</p>		
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							<img src="bliz/img/con_address.png" alt="ort" />
+						</td>
+						<td>
+							<p>Wiesenstrasse 14, 35390 Gießen</p>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							
+						</td>
+						<td>
+							<p>Gebäude: C, Räume C 302 - 307, C 309, C 320</p>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<img src="bliz/img/con_email.png" alt="email" />
+						</td>
+						<td>
+							<p><a href="mailto:bliz@thm.de">bliz@thm.de</a></p>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<img src="bliz/img/con_tel.png" alt="telefon" />
+						</td>
+						<td>
+							<p>0641 / 3 09 24 33</p>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<img src="bliz/img/con_fax.png" alt="fax" />
+						</td>
+						<td>
+							<p>0641 / 3 09 29 33</p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			
+			
 		</div>
 	</div>
 </div>
+	
