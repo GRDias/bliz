6be6c5aa267f412a9856a4b348f75fa50cbcc6b8
MarkKrauss <markuskrauss@ymail.com>
2016-01-05 14:00:32 +0100

    Content: blick ins bliz

--- sides/studium/blick_ins_bliz.php
+++ sides/studium/blick_ins_bliz.php
@@ -1,9 +1,26 @@
 <?php include "sides/mininavi_2.php"; ?>
-	<!-- Content -->
+<!-- Content -->
+<div>
+	<h1>Blick in das BliZ</h1>
+	<p>
+		 Sie erhalten hier einen Einblick in unsere beiden Arbeitsräume für unsere Studierenden.
+	</p>
+	<br />
 	
-	</br>
-	<h2>Blick in das BliZ</h2>
-	<p>Bilder werden später hinzugefügt.</p>
-	<!-- ContentEnde -->
+	<h2>
+		Raum C 304 A
+	</h2>
+	<img src="bliz/img/bilder/c-304-a-1.jpg" alt="Raum C 304 A" />
+	<img src="bliz/img/bilder/c-304-a-2.jpg" alt="Raum C 304 A" />
+	<br /><br />
+	
+	<h2>
+		Raum C 304 A
+	</h2>
+	<img src="bliz/img/bilder/c-304-b-1.jpg" alt="Raum C 304 B" />
+	<img src="bliz/img/bilder/c-304-b-2.jpg" alt="Raum C 304 B" />
+	
+</div>	
+<!-- ContentEnde -->
 <?php include "sides/studium/sidebar.php"; ?>
 
